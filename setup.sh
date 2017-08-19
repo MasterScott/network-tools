95df964422d6d3d2348b0631c404bd61bc87b14d
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-08-19 12:58:22 +0300

    Update setup script with all tools

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
 BASEDIR=$(dirname -- "$(readlink -f -- "${BASH_SOURCE}")")
 
-install_scan() {
-    cd $BASEDIR/scan_network
+scanning() {
+    cd $BASEDIR/network_scripts
     virtualenv env
     source env/bin/activate
     pip install -r requirements.txt
@@ -12,10 +12,44 @@ install_scan() {
     cd ..
 }
 
-install_bettercap() {
+bettercap() {
     sudo apt-get install build-essential ruby-dev libpcap-dev
     sudo gem install bettercap
 }
 
-install_scan
-install_bettercap
+sslstrip() {
+    cd $BASEDIR/network_scripts
+    wget https://moxie.org/software/sslstrip/sslstrip-0.9.tar.gz
+    tar zxvf sslstrip-0.9.tar.gz
+    rm sslstrip-0.9.tar.gz
+    cd ..
+    sudo apt-get install python python-twisted-web
+}
+
+echo "[*] Tools to install: "
+select INSTALL in "Bettercap & sslstrip & scanning (nmap, python)" "Bettercap" "sslstrip" "scanning (nmap, python)"
+do
+    case $INSTALL in
+        "Bettercap & sslstrip & scanning (nmap, python)")
+            sslstrip
+            bettercap
+            scanning
+            break
+            ;;
+        "Bettercap")
+            bettercap
+            break
+            ;;
+        "sslstrip")
+            sslstrip
+            break
+            ;;
+        "scanning (nmap, python)")
+            scanning
+            break
+            ;;
+        *)
+            echo "[!] Terminating installation."
+            break
+    esac
+done
